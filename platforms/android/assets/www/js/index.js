46f00d89c070a6319476ccef7385b685615db520
kurohai <kurohai@gmail.com>
2016-07-11 18:12:44 -0500

    update cordova files

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -54,60 +54,155 @@ var app = {
     },
 
     derpHarder: function() {
+        initBLE();
+
         scanDexcom();
     }
 };
 
 function checkConnected(device) {
-    ble.isConnected(
-        device.id,
-        function() {
-            alert("Peripheral is connected");
-        },
-        function() {
-            alert("Peripheral is *not* connected");
-        }
-    );
+    // Usage!
+    sleep(500).then(() => {
+        // Do something after the sleep!
+        ble.isConnected(
+            device.id,
+            function() {
+                alert("Device is connected: " + device.name);
+            },
+            function() {
+                alert("Device is *not* connected: " + device.name);
+            }
+        );
+
+    });
+
 };
+
+function foundDevice(device) {
+    // scan results
+
+    var deviceInfo = JSON.stringify(device);
+
+    console.log("found device: " + deviceInfo);
+
+    // device.services = new Uint8Array(device.advertising);
+    device.services = new Uint8Array(device.advertising);
+    device.services = toHexString(device.services);
+    // device.services = bytesToString(device.advertising);
+    var deviceInfo = JSON.stringify(device);
+
+    console.log("device ads: " + deviceInfo);
+
+    addScannedDeviceDetails(device);
+
+    checkForDexcom(device);
+};
+
+function bytesToString(buffer) {
+    return String.fromCharCode.apply(null, new Uint8Array(buffer));
+};
+
+function toHexString(byteArray) {
+    // for (i = 0; i < byteArray.length; i++) {
+    //     byte =
+    return byteArray.map(function(byte) {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('')
+};
+
+// for (i = 0; i < array.length; i++) {
+//     text += cars[i] + "<br>";
+// };
+
+
 
 function scanDexcom() {
     // do scan
-    alert("Starting Dexcom Scan");
-    ble.scan([], 360, function(device) {
-        addScannedDeviceDetails(device);
-        checkForDexcom(device);
-    }, console.log("err in scan"));
-    // var i = 1
-    // while (i == 1) {
-    //     if (checkConnected()) {
-    //         // do stuff
-    //     }
-    // }
-}
+    ble.scan([], 360, foundDevice, console.log("err in scan"));
+};
+
+// function scanDexcom2() {
+//     // do scan
+//     bluetoothle.startScan([], 360, foundDevice(device), console.log("err in scan"));
+// };
+
 
 function checkForDexcom(device) {
     // check found device for dexcom
+
     var deviceInfo = JSON.stringify(device);
-    // alert("checking: \n" + device.id + " " + device.name);
-    if ( device.name.indexOf("Dexcom") != -1 ) {
-        alert("Found Dexcom! \n" + deviceInfo);
+    if (device.name.indexOf("Dexcom") != -1) {
+        console.log("Found Dexcom! \n" + deviceInfo);
+        addScannedServiceDetails(device);
         connectDexcom(device);
         checkConnected(device);
     } else {
-        alert("not dexcom: \n" + deviceInfo);
+        console.log("not dexcom: \n" + deviceInfo);
     };
+};
+
+function connectSuccess(status) {
+    status = formatCB(status);
+    console.log("connection successful! " + status);
+};
+
+function formatCB(val) {
+    return JSON.stringify(val);
+};
+
+function connectError(status) {
+    status = formatCB(status);
+    console.log("connection error! " + status);
+};
+
+function initializeResult(status) {
+    status = formatCB(status);
+    console.log("init result: " + status);
+
+}
+
+function initBLE() {
+    params = {
+        "request": true,
+        "statusReceiver": true,
+        "restoreKey": "hellodexcom"
+    };
+    bluetoothle.initialize(initializeResult, params);
 };
 
 function connectDexcom(device) {
     // do connect
-    ble.connect(
-        device.id,
-        function() {alert("Dexcom Connected");},
-        function() {alert("Dexcom Not Connected");}
+    // not working yet
+    var params = {
+        "address": device.id
+    };
+    console.log("connection to: " + formatCB(params));
+    bluetoothle.connect(connectSuccess, connectError, params);
+
+};
+
+function scanServices(device) {
+    // do discover
+    // not working yet
+
+    var params = {
+        "address": device.id
+    }
+
+    bluetoothle.discover(
+        function(e) {
+            alert("success" + e);
+        },
+        function(e) {
+            alert("fail" + e);
+        },
+        params
     );
 };
 
 function addScannedDeviceDetails(device) {
+    // update found devices table on scan page
+
     var devicetable = document.getElementById("deviceList");
     var row = devicetable.insertRow(-1);
     var dev_id = row.insertCell(0);
@@ -118,7 +213,24 @@ function addScannedDeviceDetails(device) {
     dev_rssi.innerHTML = device.rssi;
 };
 
+function addScannedServiceDetails(device) {
+    // update found device services on scan page
 
+    var devicetable = document.getElementById("deviceFound");
+    var row = devicetable.insertRow(-1);
+    var dev_id = row.insertCell(0);
+    var dev_name = row.insertCell(1);
+    var dev_srv = row.insertCell(2);
+    dev_id.innerHTML = device.id;
+    dev_name.innerHTML = device.name;
+    dev_srv.innerHTML = '<input type="button" value="' + device.name + ' id="con_obj" />';
+};
+
+
+
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+};
 
 
 
