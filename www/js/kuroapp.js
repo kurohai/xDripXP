65292872478c38e695dc346aa57a98a70caaa6f4
Evan <kurohai@gmail.com>
2016-07-15 22:33:55 -0500

    formatting cleanup

--- www/js/kuroapp.js
+++ www/js/kuroapp.js
@@ -3,8 +3,6 @@ var kuroapp = {
         this.template_dir = "./templates/";
         this.log(this.template_dir);
         this.log("KuroApp starting...");
-        // document.getElementById("oop-btn-01").addEventListener("click", this.hello, false);
-        // $("#oop-btn-01").on("click", this.hello);
         this.bindEvents();
         this.log("KuroApp initialized!");
     },
@@ -15,15 +13,10 @@ var kuroapp = {
     },
 
     hello: function() {
-        // alert("Hello, " + name);
-        // kuroapp.setName(name);
-
         kuroapp.log("Connecting...");
     },
 
     connect: function(device) {
-        // alert("Hello, " + name);
-
         kuroapp.log("Connecting to: " + device.id);
         connectDexcom(device);
     },
@@ -37,7 +30,6 @@ var kuroapp = {
     log: function(logString) {
         // body...
         var template = "<li class=\"table-view-cell\">{{text}}</li>";
-
         console.log(logString);
         $("#oop-test-01").append(template.replace("{{text}}", logString));
     },
